0a51ca768739a9ec7e559c7579928c2b15009db7
Rander Carlos <randerccf@gmail.com>
2023-11-29 20:44:37 -0300

    improvements

--- deployment/bin/provision_server/provision_server.sh
+++ deployment/bin/provision_server/provision_server.sh
@@ -7,11 +7,13 @@ SSH_KEY=$1
 export PATH="/usr/sbin:$PATH"
 if ! id "apprunner" >/dev/null 2>&1; then
     sudo useradd -G www-data,root,sudo -u 2000 -d /home/apprunner apprunner
+    sudo usermod -aG sudo apprunner
     sudo mkdir -p /home/apprunner/.ssh
     sudo touch /home/apprunner/.ssh/authorized_keys
     sudo chown -R apprunner:apprunner /home/apprunner
+    sudo chown -R apprunner:apprunner /home/apprunner/.ssh
     sudo chown -R apprunner:apprunner /usr/src
-    sudo chmod 700 /home/apprunner/.ssh
+    sudo chmod -R 700 /home/apprunner
     sudo chmod 644 /home/apprunner/.ssh/authorized_keys
     echo "$SSH_KEY" | sudo tee -a /home/apprunner/.ssh/authorized_keys >/dev/null
 
