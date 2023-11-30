5234a2bf1ba663ff33a70ff12f7e2041078ddbdd
Rander Carlos <randerccf@gmail.com>
2023-11-30 10:08:54 -0300

    remove artisan copy and added --no-scripts

--- deployment/bin/deploy.sh
+++ deployment/bin/deploy.sh
@@ -7,9 +7,10 @@ cd /usr/src
 sudo docker-compose -f docker-compose.prod.yml down
 sudo docker-compose -f docker-compose.prod.yml up -d --remove-orphans
 
+sudo docker-compose -f docker-compose.prod.yml exec -T api php artisan package:discover --ansi
+sudo docker-compose -f docker-compose.prod.yml exec -T api php artisan vendor:publish --tag=laravel-assets --ansi --force
 sudo docker-compose -f docker-compose.prod.yml exec -T api php artisan migrate --force
 sudo docker-compose -f docker-compose.prod.yml exec -T api php artisan config:cache
 sudo docker-compose -f docker-compose.prod.yml exec -T api php artisan route:cache
 sudo docker-compose -f docker-compose.prod.yml exec -T api php artisan view:cache
 sudo docker-compose -f docker-compose.prod.yml exec -T api php artisan event:cache
-
