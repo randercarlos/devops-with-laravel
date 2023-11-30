98f1499be845e33632e23c0afdf5db1bb5cd19ce
Rander Carlos <randerccf@gmail.com>
2023-11-30 10:43:29 -0300

    added db:seed --force step in deploy

--- deployment/bin/deploy.sh
+++ deployment/bin/deploy.sh
@@ -10,6 +10,7 @@ sudo docker-compose -f docker-compose.prod.yml up -d --remove-orphans
 sudo docker-compose -f docker-compose.prod.yml exec -T api php artisan package:discover --ansi
 sudo docker-compose -f docker-compose.prod.yml exec -T api php artisan vendor:publish --tag=laravel-assets --ansi --force
 sudo docker-compose -f docker-compose.prod.yml exec -T api php artisan migrate --force
+sudo docker-compose -f docker-compose.prod.yml exec -T api php db:seed --force
 sudo docker-compose -f docker-compose.prod.yml exec -T api php artisan config:cache
 sudo docker-compose -f docker-compose.prod.yml exec -T api php artisan route:cache
 sudo docker-compose -f docker-compose.prod.yml exec -T api php artisan view:cache
