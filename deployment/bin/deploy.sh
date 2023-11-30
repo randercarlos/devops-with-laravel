fd293ecc8d1e232313d5e55c2c2c493fade23324
Rander Carlos <randerccf@gmail.com>
2023-11-30 09:34:03 -0300

    add migrate and event:cache commands in deploy.sh

--- deployment/bin/deploy.sh
+++ deployment/bin/deploy.sh
@@ -7,6 +7,9 @@ cd /usr/src
 sudo docker-compose -f docker-compose.prod.yml down
 sudo docker-compose -f docker-compose.prod.yml up -d --remove-orphans
 
+sudo docker-compose -f docker-compose.prod.yml exec -T api php artisan migrate --force
 sudo docker-compose -f docker-compose.prod.yml exec -T api php artisan config:cache
 sudo docker-compose -f docker-compose.prod.yml exec -T api php artisan route:cache
 sudo docker-compose -f docker-compose.prod.yml exec -T api php artisan view:cache
+sudo docker-compose -f docker-compose.prod.yml exec -T api php artisan event:cache
+
