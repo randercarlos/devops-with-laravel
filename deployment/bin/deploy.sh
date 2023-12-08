69b58b6b7d36d76ee433657350b97349a5854576
Rander Carlos <randerccf@gmail.com>
2023-12-08 19:12:13 -0300

    refactor: remove unused steps and improvements

--- deployment/bin/deploy.sh
+++ deployment/bin/deploy.sh
@@ -9,7 +9,7 @@ sudo docker-compose -f docker-compose.prod.yml up -d
 
 sudo docker-compose -f docker-compose.prod.yml exec -T api php artisan migrate --force
 sudo docker-compose -f docker-compose.prod.yml exec -T api php artisan db:seed --force
-sudo docker-compose -f docker-compose.prod.yml exec -T api php artisan config:cache
-sudo docker-compose -f docker-compose.prod.yml exec -T api php artisan route:cache
-sudo docker-compose -f docker-compose.prod.yml exec -T api php artisan view:cache
-sudo docker-compose -f docker-compose.prod.yml exec -T api php artisan event:cache
+sudo docker-compose -f docker-compose.prod.yml exec -T api php artisan optimize:clear
+sudo docker-compose -f docker-compose.prod.yml exec -T api php artisan optimize
+sudo docker-compose -f docker-compose.prod.yml exec -T api rm public/storage
+sudo docker-compose -f docker-compose.prod.yml exec -T api php artisan storage:link
