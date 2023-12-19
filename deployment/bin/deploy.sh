2d60d0c9766834224f9780c4ea184066a644168a
Rander Carlos <randerccf@gmail.com>
2023-12-19 11:30:23 -0300

    FIX: CICD

--- deployment/bin/deploy.sh
+++ deployment/bin/deploy.sh
@@ -9,7 +9,6 @@ sudo docker-compose -f docker-compose.prod.yml up -d
 
 sudo docker-compose -f docker-compose.prod.yml exec -T api php artisan migrate --force
 sudo docker-compose -f docker-compose.prod.yml exec -T api php artisan db:seed --force
-sudo docker-compose -f docker-compose.prod.yml exec -T api php artisan optimize:clear
 sudo docker-compose -f docker-compose.prod.yml exec -T api php artisan optimize
 sudo docker-compose -f docker-compose.prod.yml exec -T api rm -f public/storage
 sudo docker-compose -f docker-compose.prod.yml exec -T api php artisan storage:link
