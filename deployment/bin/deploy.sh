96a52c63eb46bf3e3d50818693e06aafccefd43c
Rander Carlos <randerccf@gmail.com>
2023-12-20 16:56:50 -0300

    chore: improvement

--- deployment/bin/deploy.sh
+++ deployment/bin/deploy.sh
@@ -9,8 +9,9 @@ sudo docker image prune -f --filter "until=48h" # remove images old more 2 days 
 sudo docker-compose -f docker-compose.prod.yml down --remove-orphans
 sudo docker-compose -f docker-compose.prod.yml up -d
 
-sudo docker-compose -f docker-compose.prod.yml exec -T api php artisan migrate --force
-sudo docker-compose -f docker-compose.prod.yml exec -T api php artisan db:seed --force
+sudo docker-compose -f docker-compose.prod.yml exec -T api php artisan optimize:clear
 sudo docker-compose -f docker-compose.prod.yml exec -T api php artisan optimize
 sudo docker-compose -f docker-compose.prod.yml exec -T api rm -f public/storage
 sudo docker-compose -f docker-compose.prod.yml exec -T api php artisan storage:link
+sudo docker-compose -f docker-compose.prod.yml exec -T api php artisan migrate --force
+sudo docker-compose -f docker-compose.prod.yml exec -T api php artisan db:seed --force
