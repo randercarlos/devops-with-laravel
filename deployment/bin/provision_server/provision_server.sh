4a2234acc8292ae79b2a74d14859bceb37a9ab98
Rander Carlos <randerccf@gmail.com>
2023-12-23 19:48:38 -0300

    chore: remove windows LR

--- deployment/bin/provision_server/provision_server.sh
+++ deployment/bin/provision_server/provision_server.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+
 set -e
 SSH_KEY=$1
+
 export PATH="/usr/sbin:$PATH"
+
 if ! id "apprunner" >/dev/null 2>&1; then
     sudo useradd -G www-data,root,sudo -u 2000 -d /home/apprunner apprunner
     sudo usermod -aG sudo apprunner
@@ -15,23 +18,28 @@ if ! id "apprunner" >/dev/null 2>&1; then
     echo "$SSH_KEY" | sudo tee -a /home/apprunner/.ssh/authorized_keys >/dev/null
     sudo bash -c 'echo "apprunner ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers.d/apprunner'
 fi
+
 # Install docker
 sudo apt-get update -y
 sudo apt-get install unzip -y
 sudo curl -fsSL https://get.docker.com/ | sh
 sudo usermod -aG docker apprunner
 docker --version
+
 # Install docker-compose
 sudo curl -L "https://github.com/docker/compose/releases/download/v2.19.1/docker-compose-linux-x86_64" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 docker-compose --version
+
 # Install AWS CLI
 sudo curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" && \
     unzip -o awscliv2.zip
 sudo ./aws/install --update
 sudo rm -f awscliv2.zip
+
 PATH="/root/.local/bin:${PATH}"
-# Definindo códigos de cor ANSI
+
+# Definindo codigos de cor ANSI
 verde='\033[0;32m'
 reset='\033[0m'
 echo -e "${verde}Provision was successfully executed!${reset}"
